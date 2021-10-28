5ca07ea5dae106efe69ee85ad5b6e932f2fe7aa0
Sean L <splambert1020@gmail.com>
2021-10-28 15:27:57 -0400

    initial commit

---
+++ Lab21/Controllers/MovieRegistrationController.cs
@@ -0,0 +1,110 @@
+﻿using Lab21.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21.Controllers
+{
+    public class MovieList
+    {
+        public static List<Movie> Movies = new List<Movie>();
+    }
+
+    public class MovieRegistrationController : Controller
+    {
+        private List<Movie> _allMovies;
+
+        public MovieRegistrationController()
+        {
+            _allMovies = MovieList.Movies;
+        }
+
+        public List<Movie> testMovies;
+
+        // GET: MovieRegistrationController
+        public ActionResult Index()
+        {
+            return View(_allMovies);
+        }
+
+        public ActionResult IndexSpecific(Movie newMovie)
+        {
+            ViewData["Message"] = $"{newMovie.Title} has been successfully registered.";
+            return View("Index", _allMovies);
+        }
+
+        // GET: MovieRegistrationController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: MovieRegistrationController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MovieRegistrationController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind("ID", "Title", "Genre", "Year", "Actors", "Directors")]Movie newMovie)
+        {
+            try
+            {
+                MovieList.Movies.Add(newMovie);
+                
+                return RedirectToAction(nameof(IndexSpecific), newMovie);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieRegistrationController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieRegistrationController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MovieRegistrationController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: MovieRegistrationController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Lab21/Models/Movie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab21.Models
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+
+        [MaxLength(50, ErrorMessage = "Title cannot be longer than 50 characters.")]
+        public string Title { get; set; }
+
+        public string Genre { get; set; }
+
+        [Range(1880, 2021, ErrorMessage = "Year cannot be before 1880 or after 2021.")]
+        public int Year { get; set; }
+
+        public string Actors { get; set; }
+
+        public string Directors { get; set; }
+    }
+}
